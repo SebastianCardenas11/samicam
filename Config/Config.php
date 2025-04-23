cf77d49e0349c3050d9da983552087a7173bd57c
Carlos David Lopez <carloslxpxz@gmail.com>
2025-04-23 16:16:49 -0500

    Update
    creacion del la vista del modulo de funcionarios ops

--- Config/Config.php
+++ Config/Config.php
@@ -5,8 +5,8 @@ const BASE_URL = "http://localhost/samicam";
 date_default_timezone_set('America/Bogota');
 
 //Datos de conexión a Base de Datos
-const DB_HOST = "localhost:3307";
-const DB_NAME = "sigma1";
+const DB_HOST = "localhost:3306";
+const DB_NAME = "samicam";
 const DB_USER = "root";
 const DB_PASSWORD = "";
 const DB_CHARSET = "utf8";
---
+++ Controllers/FuncionariosOps.php
@@ -0,0 +1,28 @@
+<?php
+
+class FuncionariosOps extends Controllers
+{
+    public function __construct()
+    {
+        parent::__construct();
+        session_start();
+        //session_regenerate_id(true);
+        if (empty($_SESSION['login'])) {
+            header('Location: ' . base_url() . '/login');
+            die();
+        }
+        getPermisos(MDASHBOARD);
+    }
+
+    public function FuncionariosOps()
+    {
+        if (empty($_SESSION['permisosMod']['r'])) {
+            header("Location:" . base_url() . '/dashboard');
+        }
+        $data['page_tag'] = "Funcionarios Ops";
+        $data['page_title'] = "Funcionarios Ops";
+        $data['page_name'] = "Funcionarios Ops";
+        $data['page_functions_js'] = "functions_funcionariosops.js";
+        $this->views->getView($this, "funcionariosops", $data);
+    }
+}
